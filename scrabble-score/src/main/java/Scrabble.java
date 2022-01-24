452d6cf14181e87fa470073ae0da766c000c45d3
Thanawat Chuensriwirot <thechaingza999@gmail.com>
2022-01-24 21:16:27 +0700

    add Scrabble

---
+++ scrabble-score/src/main/java/Scrabble.java
@@ -0,0 +1,24 @@
+class Scrabble {
+    int score = 0;
+    Scrabble(String word) {
+        if(word.length()!=0) {
+            for (int i = 0; i < word.length(); i++) {
+                char letter = Character.toUpperCase(word.charAt(i));
+                switch(letter){
+                    case 'A': case 'E': case 'I': case 'O': case 'U': case 'L': case 'N': case 'R': case 'S': case 'T':score+=1;break;
+                    case 'D': case 'G': score+=2;break;
+                    case 'B': case 'C': case 'M': case 'P':score+=3;break;
+                    case 'F':case 'H':case 'V':case 'W':case 'Y':score+=4;break;
+                    case 'K':score+=5;break;
+                    case 'J':case 'X':score+=8;break;
+                    case 'Q':case 'Z':score+=10;break;
+                }
+            }
+        }
+    }
+
+    int getScore() {
+        return score;
+    }
+
+}
